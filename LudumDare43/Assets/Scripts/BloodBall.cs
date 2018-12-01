5ded777a1a8f084b1ba74d04e4136f9b5a57fbb1
Omari Straker <straka3000@gmail.com>
2018-11-30 23:26:54 -0800

    add player basic spellbook and generic projectile blood spell

---
+++ LudumDare43/Assets/Scripts/BloodBall.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodBall : MonoBehaviour {
+
+
+   public void ShootProjectile(Vector3 velocity)
+    {
+        GetComponent<Rigidbody>().velocity = velocity;
+    }
+}
---
+++ LudumDare43/Assets/Scripts/SpellBook.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellBook : MonoBehaviour {
+
+    public GameObject BloodBallPrefab;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            BloodBall();
+        }
+	}
+
+    void BloodBall()
+    {
+        Vector3 spawnPosition = transform.position + new Vector3(0, 1.5f, 0);
+        GameObject createdObj =  Instantiate(BloodBallPrefab, spawnPosition,Quaternion.identity);
+        createdObj.GetComponent<BloodBall>().ShootProjectile(new Vector3(5, 0, 5));
+    }
+}
