27586777684a4dcdfc6a1e778d32113bcaab2312
Noah Malik <iamn3arl@gmail.com>
2018-12-01 11:20:00 -0800

    Added script structure for player health and blood sacrifice with idol.

--- LudumDare43/Assets/Scripts/Idol.cs
+++ LudumDare43/Assets/Scripts/Idol.cs
@@ -2,39 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Idol : MonoBehaviour {
+public class Idol : MonoBehaviour
+{
 
-    bool showLabel = false;
-	// Use this for initialization
-	void Start () {
+    bool playerInRange = false;  // Player has entered sacrifice area (sphere collider)
+	
+    // Use this for initialization
+	void Start()
+    {
 		
 	}
 
     private void OnGUI()
     {
-        if (showLabel)
+        if (playerInRange)
         {
             GUI.Label(new Rect(Screen.width-200, 50, 400, 100), "Press X to Pay Tribute");
         }
     }
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 		
 	}
 
+    // On Enter with Sphere Collider Trigger
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Player"))
         {
-            showLabel = true;
+            playerInRange = true;
         }
     }
 
+    // On Exit from Sphere Collider Trigger
     private void OnTriggerExit(Collider other)
     {
         if (other.tag.Equals("Player"))
         {
-            showLabel = false;
+            playerInRange = false;
         }
     }
 }
---
+++ LudumDare43/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+	public int startHealth = 100;                      // Starting player health
+	public int currentHealth;                          // Current player health
+	public Slider healthSlider;                        // Ref to UI health bar.
+	public Image damageImage;                          // Ref to image that flashes on player damage
+	public float flashspeed = 5f;                      // Speed that image damage image fades at
+	public Color flashColor = new Color(1f, 0f, 0.1f); // Color of damage image to flash
+	public AudioClip deathClip;
+
+	Animator anim;                                     // Ref to Animator component
+	AudioSource playerAudio;                            // Ref to AudioSource component
+	//PlayerMovement playerMovement;                     // NEED REF TO PLAYER'S MOVEMENT SCRIPT??
+	//SpellBook spellBook;     NEED REFERENCE TO PLAYER SPELLBOOK (damage dealing) SCRIPT?????
+	bool isDead;                                       // Whether player is dead
+	bool damaged;                                      // When the player gets damaged
+
+	// Use this for initialization
+	void Awake()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
