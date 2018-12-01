0b3b6e73b4df101bf365927d57b55117344d1553
Omari Straker <straka3000@gmail.com>
2018-11-30 22:16:39 -0800

    Add Idol script and trigger gui when player enters tribute area

---
+++ LudumDare43/Assets/Scripts/Idol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idol : MonoBehaviour {
+
+    bool showLabel = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnGUI()
+    {
+        if (showLabel)
+        {
+            GUI.Label(new Rect(Screen.width-200, 50, 400, 100), "Press X to Pay Tribute");
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            showLabel = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            showLabel = false;
+        }
+    }
+}
