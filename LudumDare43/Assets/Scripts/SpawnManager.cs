67a8c985c285af44b48a237af7099ae016791639
Edwin T <edwint@Edwins-MBP.internal.lan>
2018-12-01 14:16:35 -0800

    Add peasants, navigation, and some more terrain

--- LudumDare43/Assets/Scripts/SpawnManager.cs
+++ LudumDare43/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour {
 
     //public PlayerHealth playerHealth;       // Reference to the player's heatlh.
-    public GameObject enemy;                // The enemy prefab to be spawned.
+    public GameObject[] enemyType;                // The enemy prefab to be spawned.
     public float spawnTime = 10f;            // How long between each spawn.
     public Transform[] spawnPoints;         // An array of the spawn points this enemy can spawn from.
 
@@ -28,8 +28,9 @@ public class SpawnManager : MonoBehaviour {
 
         // Find a random index between zero and one less than the number of spawn points.
         int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        int enemyTypeIndex = Random.Range(0, enemyType.Length);
         Vector3 randomizedOffset = Random.insideUnitCircle;
         // Create an instance of the enemy prefab at the randomly selected spawn point's position and rotation.
-        Instantiate(enemy, spawnPoints[spawnPointIndex].position + randomizedOffset, spawnPoints[spawnPointIndex].rotation);
+        Instantiate(enemyType[enemyTypeIndex], spawnPoints[spawnPointIndex].position + randomizedOffset, spawnPoints[spawnPointIndex].rotation);
     }
 }
