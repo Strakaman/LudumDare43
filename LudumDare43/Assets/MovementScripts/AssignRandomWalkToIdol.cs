f401f1ef70985be81bfb65fab54e3149e345c13b
Edwin T <edwint@Edwins-MBP.internal.lan>
2018-12-01 15:54:11 -0800

    Add more spawn points and random walk for enemies

---
+++ LudumDare43/Assets/MovementScripts/AssignRandomWalkToIdol.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignRandomWalkToIdol : MonoBehaviour {
+    RandomWalk randomWalk;
+
+	// Use this for initialization
+	void Start () {
+        randomWalk = GetComponent<RandomWalk>();
+        Transform idolLocation = GameObject.FindWithTag("Idol").GetComponent<Transform>();
+        randomWalk.stayAround = idolLocation;
+	}
+}
