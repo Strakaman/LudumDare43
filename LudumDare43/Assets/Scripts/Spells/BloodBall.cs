e30f995a7175c3bb1c3a1d40f9a2fdcb1b8ff4fd
Noah Malik <iamn3arl@gmail.com>
2018-12-01 20:59:26 -0800

    Blood spell gets cast in the correct direction from player!

--- LudumDare43/Assets/Scripts/Spells/BloodBall.cs
+++ LudumDare43/Assets/Scripts/Spells/BloodBall.cs
@@ -2,20 +2,6 @@
 
 public class BloodBall : ISpell
 {
-	/*public override int cooldown
-	{
-		get
-		{
-			return cooldown;
-		}
-
-		set
-		{
-			cooldown = value;
-		}
-	}*/
-
-	//public int cooldown = 10;
 	public int damage = 10;
 
 	public GameObject BloodProjectile;
@@ -27,43 +13,12 @@ public class BloodBall : ISpell
 
 	public override void Execute()
 	{
-
-		Vector3 spawnPosition = transform.position + new Vector3(0f, 1.5f, -1f);
-
-		GameObject createProjectile = Instantiate(BloodProjectile, spawnPosition, Quaternion.identity);
+		Quaternion spawnRotation = transform.rotation;
+		Vector3 spawnPosition = transform.position + spawnRotation * (new Vector3(0f, 1.5f, -1f));
 		
-		createProjectile.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, -5f);
+		GameObject createProjectile = Instantiate(BloodProjectile, spawnPosition, spawnRotation);
+
+		createProjectile.GetComponent<Rigidbody>().velocity = spawnRotation	* (new Vector3(0f, 0f, -5f));
 	}
 
-
-	/*
-   public void ShootProjectile(Vector3 velocity)
-    {
-        GetComponent<Rigidbody>().velocity = velocity;
-    }*/
 }
-
-// CODE DELETED FROM SPELLBOOK.CS IN CASE WE WANT TO SAVE IT
-
-/*
-public GameObject BloodBallPrefab;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetButtonDown("Fire1"))
-        {
-            BloodBall();
-        }
-	}
-
-    void BloodBall()
-    {
-        Vector3 spawnPosition = transform.position + new Vector3(0, 1.5f, 0);
-        GameObject createdObj =  Instantiate(BloodBallPrefab, spawnPosition,Quaternion.identity);
-        createdObj.GetComponent<BloodBall>().ShootProjectile(new Vector3(5, 0, 5));
-    }
-*/
