6061953aed7ebfbf7eaa4de679c8124d9344f8fb
dalecr <crdale42@gmail.com>
2018-12-02 14:36:37 -0800

    Fixed the train for reals this time

--- LudumDare43/Assets/Scripts/Spells/MakeItTrain.cs
+++ LudumDare43/Assets/Scripts/Spells/MakeItTrain.cs
@@ -14,11 +14,11 @@ public class MakeItTrain : ISpell
     public override void Execute()
     {
         Quaternion spawnRotation = transform.rotation;
-        Vector3 spawnPosition = transform.position + spawnRotation * (new Vector3(0f, -10f, -60f));
+        Vector3 spawnPosition = transform.position + spawnRotation * (new Vector3(0f, -10f, -25f));
 
         GameObject createProjectile = Instantiate(TrainDrops, spawnPosition, spawnRotation);
 
-        createProjectile.GetComponent<Rigidbody>().velocity = spawnRotation * (new Vector3(0f, 0f, 30f));
+        createProjectile.GetComponent<Rigidbody>().velocity = spawnRotation * (new Vector3(0f, 0f, 0f));
     }
 
 }
