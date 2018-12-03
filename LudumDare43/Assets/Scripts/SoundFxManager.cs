529f2df40db8189af2a3aecf897aadc8f4196c7e
Noah Malik <iamn3arl@gmail.com>
2018-12-02 16:46:21 -0800

    Sound FX Manager script to add to our loud game objects.

---
+++ LudumDare43/Assets/Scripts/SoundFxManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SoundFxManager : MonoBehaviour
+{
+
+    public AudioClip[] audioClips;
+    public AudioSource audioSource;
+
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+
+    public void PlayRandom()
+    {
+        audioSource.clip = audioClips[Mathf.RoundToInt(Random.Range(0f, (audioClips.Length - 1)))];
+        audioSource.Play();
+    }
+}
